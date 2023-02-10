dd4ab4fbf8354a05638a69f6faae6bda7db01580
maxim <maximmilshtien@gmail.com>
2023-02-10 13:29:59 +0200

    refactor login-page

--- src/page-objects/login-page/LoginPage-page.ts
+++ src/page-objects/login-page/LoginPage-page.ts
@@ -4,17 +4,19 @@ import * as data from '../../data/data.json';
 import { BasePage } from '../abstract-page/base-page';
 
 export class LoginPage extends BasePage<LoginPageLocatorsType, LoginPageFrameLocatorsType> {
+  private readonly pageData = data.LOGIN_PAGE;
+
   constructor(options: OptionsType) {
     super(options);
   }
 
   public async visit(): Promise<void> {
-    await this.page.goto(data.LOGIN_PAGE.URL);
+    await this.page.goto(this.pageData.URL);
   }
 
   public async fillUserNameAndPassword(): Promise<void> {
-    await this.locators.USERNAME_INPUT.fill(data.LOGIN_PAGE.USERNAME);
-    await this.locators.PASSWORD_INPUT.fill(data.LOGIN_PAGE.PASSWORD);
+    await this.locators.USERNAME_INPUT.fill(this.pageData.USERNAME);
+    await this.locators.PASSWORD_INPUT.fill(this.pageData.PASSWORD);
     await this.locators.LOGIN_BUTTON.click();
   }
 }
